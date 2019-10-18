75ef8ed545ba78f780eaa1b3c39a9315601672d2
Maurits van der Schee <maurits@vdschee.nl>
2019-10-18 14:48:55 +0200

    initial commit

---
+++ src/SimplePdo.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Tqdev\PdoJson;
+
+class SimplePdo extends LazyPDO
+{
+
+    public function insert($table, $record): int
+    {
+        if (empty($table) || empty($record)) {
+            return 0;
+        }
+        $params = [];
+        $sql = 'INSERT INTO ' . $this->quoteIdentifier($table) . ' ';
+        $sql .= $this->buildSqlInsertFields($record) . ' VALUES ';
+        $sql .= $this->buildSqlInsertValues($params, $record) . ' ';
+        return $this->q($sql, $params, false, true);
+    }
+
+    public function select($table, $fields = [], $conditions = []): array
+    {
+        if (empty($table)) {
+            return [];
+        }
+        $params = [];
+        $sql = 'SELECT ' . $this->buildSqlSelect($fields) . ' ';
+        $sql .= 'FROM ' . $this->quoteIdentifier($table) . ' ';
+        $sql .= $this->buildSqlWhere($params, $conditions);
+        return $this->q($sql, $params, false, false);
+    }
+
+    public function update($table, $fields, $conditions): int
+    {
+        if (empty($table) || empty($fields) || empty($conditions)) {
+            return 0;
+        }
+        $params = [];
+        $sql = 'UPDATE ' . $this->quoteIdentifier($table) . ' ';
+        $sql .= 'SET ' . $this->buildSqlSet($params, $fields) . ' ';
+        $sql .= $this->buildSqlWhere($params, $conditions);
+        return $this->q($sql, $params, true, false);
+    }
+
+    public function delete($table, $conditions): int
+    {
+        if (empty($table) || empty($conditions)) {
+            return 0;
+        }
+        $params = [];
+        $sql = 'DELETE FROM ' . $this->quoteIdentifier($table) . ' ';
+        $sql .= $this->buildSqlWhere($params, $conditions);
+        return $this->q($sql, $params, true);
+    }
+
+    public function quoteIdentifier(string $string): string
+    {
+        $str = \preg_replace('/[^\.0-9a-zA-Z_\/]/', '', $string);
+        if (\strpos($str, '.') !== false) {
+            $parts = \explode('.', $str);
+            foreach ($parts as $i => $part) {
+                $parts[$i] = $this->quoteIdentifier($part);
+            }
+            return \implode('.', $parts);
+        }
+        return '"' . $str . '"';
+    }
+
+    protected function buildSqlInsertFields(array $record): string
+    {
+        $names = [];
+        foreach (array_keys($record) as $name) {
+            $names[] = $this->quoteIdentifier($name);
+        }
+        return '(' . \implode(', ', $names) . ')';
+    }
+
+    protected function buildSqlInsertValues(array &$params, array $record): string
+    {
+        $args = [];
+        foreach ($record as $value) {
+            if ($value === null) {
+                $args[] = 'NULL';
+            } elseif (\is_bool($value)) {
+                $args[] = $this->makeBool($value);
+            } else {
+                $args[] = '?';
+                $params[] = $value;
+            }
+        }
+        return '(' . \implode(', ', $args) . ')';
+    }
+
+    protected function buildSqlSelect(array $fields): string
+    {
+        if (empty($fields)) {
+            return '*';
+        }
+        $args = [];
+        foreach ($fields as $name) {
+            $name = $this->quoteIdentifier($name);
+            $args[] = $name;
+        }
+        return \implode(', ', $fields);
+    }
+
+    protected function buildSqlSet(array &$params, array $fields): string
+    {
+        $args = [];
+        foreach ($fields as $name => $value) {
+            $name = $this->quoteIdentifier($name);
+            if ($value === null) {
+                $args[] = $name . ' = NULL';
+            } elseif (\is_bool($value)) {
+                $args[] = $name . ' = ' . $this->makeBool($value);
+            } else {
+                $args[] = $name . ' = ?';
+                $params[] = $value;
+            }
+        }
+        return \implode(', ', $args);
+    }
+
+    protected function selectOperator(string $operator): string
+    {
+        $operators = ['', '=', '>', '<', '>=', '<=', '<>', 'IS', 'IN', 'IS NOT', 'NOT IN', 'LIKE', 'NOT LIKE'];
+        return in_array($operator, $operators) ? $operator : '';
+    }
+
+    protected function buildSqlWhere(array &$params, array $conditions): string
+    {
+        if (empty($conditions)) {
+            return '';
+        }
+        $args = [];
+        foreach ($conditions as $name => $value) {
+            $parts = explode(' ', $name, 2);
+            if (count($parts) != 2) {
+                $parts = [$parts[0], ''];
+            }
+            list($name, $operator) = $parts;
+            $name = $this->quoteIdentifier($name);
+            $operator = $this->selectOperator($operator);
+            if ($value === null) {
+                $operator = $operator ?: 'IS';
+                $args[] = $name . ' ' . $operator . ' NULL';
+            } elseif (\is_array($value)) {
+                $operator = $operator ?: 'IN';
+                $qmarks = explode(', ', str_split(str_repeat('?', count($value))));
+                $args[] = $name . ' ' . $operator . ' (' . $qmarks . ')';
+                foreach ($value as $val) {
+                    $params[] = $val;
+                }
+            } elseif (\is_bool($value)) {
+                $operator = $operator ?: '=';
+                $args[] = $name . ' ' . $operator . ' ' . $this->makeBool($value);
+            } else {
+                $operator = $operator ?: '=';
+                $args[] = $name . ' ' . $operator . ' ?';
+                $params[] = $value;
+            }
+        }
+        return 'WHERE ' . \implode(' AND ', $args);
+    }
+
+    protected function makeBool(bool $value): string
+    {
+        switch ($this->driver) {
+            case 'sqlite':
+                return $value ? '1' : '0';
+            default:
+                return $value ? 'TRUE' : 'FALSE';
+        }
+    }
+}
---
+++ src/SmartPdo.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tqdev\PdoJson;
+
+class SmartPdo extends LazyPDO
+{
+    protected $driver;
+    protected $options;
+    protected $commands;
+
+    public function __construct(string $dsn, string $username = null, string $password = null, array $options = null)
+    {
+        $this->driver = strtolower(substr($dsn, 0, strpos($dsn, ':')));
+        if (!in_array($this->driver, ['mysql', 'pgsql', 'sqlsrv'])) {
+            throw new \Exception(sprintf('Driver "%s" is not supported', $this->driver));
+        }
+        $this->options = $this->getOptions($options ?: array());
+        $this->commands = $this->getCommands(array());
+        foreach ($this->commands as $command) {
+            $this->addInitCommand($command);
+        }
+        parent::__construct($dsn, $username, $password, $this->options);
+    }
+
+    public function q(string $statement, array $params = [], bool $returnNumAffected = false, bool $returnLastInsertId = false)
+    {
+        if (empty($params)) {
+            $stmt = $this->pdo->query($statement);
+        } else {
+            $stmt = $this->prepare($statement);
+            $stmt->execute($params);
+        }
+        if ($returnNumAffected) {
+            return $stmt->rowCount();
+        } else if ($returnLastInsertId) {
+            return $this->lastInsertId();
+        }
+        return $stmt->fetchAll();
+    }
+
+    protected function getCommands(array $commands): array
+    {
+        switch ($this->driver) {
+            case 'mysql':
+                return $commands + [
+                    'SET SESSION sql_warnings=1;',
+                    'SET NAMES utf8mb4;',
+                    'SET SESSION sql_mode = "ANSI,TRADITIONAL";',
+                ];
+            case 'pgsql':
+                return $commands + [
+                    "SET NAMES 'UTF8';",
+                ];
+        }
+        return $commands;
+    }
+
+    protected function getOptions(array $options): array
+    {
+        $options += array(
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        );
+        switch ($this->driver) {
+            case 'mysql':
+                return $options + [
+                    \PDO::ATTR_EMULATE_PREPARES => false,
+                    \PDO::MYSQL_ATTR_FOUND_ROWS => true,
+                    \PDO::ATTR_PERSISTENT => true,
+                ];
+            case 'pgsql':
+                return $options + [
+                    \PDO::ATTR_EMULATE_PREPARES => false,
+                    \PDO::ATTR_PERSISTENT => true,
+                ];
+            case 'sqlsrv':
+                return $options + [
+                    \PDO::SQLSRV_ATTR_DIRECT_QUERY => false,
+                    \PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE => true,
+                ];
+        }
+        return $options;
+    }
+}
