1db3c5b9f60ffab52a30c24adcb8c5c9896fc8ac
Maurits van der Schee <maurits@vdschee.nl>
2019-10-19 12:50:00 +0200

    split test classes

--- tests/SimplePdoTest.php
+++ tests/SimplePdoTest.php
@@ -1,49 +1,16 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Tqdev\PdoJson\PathPdo;
+use Tqdev\PdoJson\SimplePdo;
 
 class SimplePdoTest extends TestCase
 {
-    /**
-     * @dataProvider qDataProvider
-     */
-    public function testQ($a, $b, $expected)
-    {
-        $db = PathPdo::create('php-crud-api', 'php-crud-api', 'php-crud-api');
-        $this->assertSame($expected, json_encode($db->q($a, $b)));
-    }
-
-    public function qDataProvider()
-    {
-        return [
-            'single record' => ['select id, content from posts where id=?', [1], '[{"id":1,"content":"blog started"}]'],
-            'two records' => ['select id from posts where id<=2', [], '[{"id":1},{"id":2}]'],
-            'two records named' => ['select id from posts where id<=:two and id>=:one', ['one' => 1, 'two' => 2], '[{"id":1},{"id":2}]'],
-            'posts with comments' => [
-                'select posts.id as "posts[].id", comments.id as "posts[].comments[].id" from posts left join comments on post_id = posts.id where posts.id<=2', [],
-                '{"posts":[{"id":1,"comments":[{"id":1},{"id":2}]},{"id":2,"comments":[{"id":3},{"id":4},{"id":5},{"id":6}]}]}'
-            ],
-            'comments with post' => [
-                'select posts.id as "comments[].post.id", comments.id as "comments[].id" from posts left join comments on post_id = posts.id where posts.id<=2', [],
-                '{"comments":[{"id":1,"post":{"id":1}},{"id":2,"post":{"id":1}},{"id":3,"post":{"id":2}},{"id":4,"post":{"id":2}},{"id":5,"post":{"id":2}},{"id":6,"post":{"id":2}}]}'
-            ],
-            'count posts' => ['select count(*) as "posts" from posts', [], '[{"posts":12}]'],
-            'count posts object' => ['select count(*) as ".posts" from posts', [], '{"posts":12}'],
-            'count posts with root' => ['select count(*) as "statistics.posts" from posts', [], '{"statistics":{"posts":12}}'],
-            'count posts and comments' => [
-                'select (select count(*) from posts) as "stats.posts", (select count(*) from comments) as "stats.comments"', [],
-                '{"stats":{"posts":12,"comments":6}}'
-            ],
-        ];
-    }
-
     /**
      * @dataProvider selectDataProvider
      */
     public function testSelect($a, $b, $c, $expected)
     {
-        $db = PathPdo::create('php-crud-api', 'php-crud-api', 'php-crud-api');
+        $db = SimplePdo::create('php-crud-api', 'php-crud-api', 'php-crud-api');
         $this->assertSame($expected, json_encode($db->select($a, $b, $c)));
     }
 
